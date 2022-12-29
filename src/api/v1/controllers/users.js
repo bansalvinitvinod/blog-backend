19eb435b43152474cd1ef6ab1586ef3a298e1a18
Vinit Bansal <vinitbansal1998@gmail.com>
2022-12-29 13:04:37 +0530

    Users controller syntactical fixes made

--- src/api/v1/controllers/users.js
+++ src/api/v1/controllers/users.js
@@ -1,32 +1,32 @@
 const services = require('../services');
 
-const getUsers = (async (req, res) => {
+const getUsers = async (req, res) => {
     res.status(200).json(await services.users.getAll());
-})
+}
 
-const getUser = (async (req, res) => {
+const getUser = async (req, res) => {
     const id = Number(req.params.id);
     const User = await services.users.get(id);
     if (!User) {
-        return res.status(404).send('User not found')
+        return res.status(404).send('User not found');
     }
     res.json(User);
-})
+}
 
-const createUser = (async (req, res) => {
+const createUser = async (req, res) => {
     const newUser = req.body;
     await services.users.create(newUser);
     res.status(201).json(newUser);
-})
+}
 
-const updateUser = (async (req, res) => {
+const updateUser = async (req, res) => {
     const id = Number(req.params.id);
     const updated = await services.users.update(id, req.body);
     if(!updated) {
         res.status(404).json('User not updated');
     }
-    res.status(200).json('User updated')
-})
+    res.status(200).json('User updated');
+}
 
 module.exports = {
     getUsers,
