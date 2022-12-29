b555aaa6fb8b5cb25515f0ededd186b43c8d1a11
Vinit Bansal <vinitbansal1998@gmail.com>
2022-12-29 13:07:05 +0530

    Roles service syntactical fixes made

--- src/api/v1/services/roles.js
+++ src/api/v1/services/roles.js
@@ -1,34 +1,34 @@
 const models = require('../models')
 
-const getAll = (async () => {
+const getAll = async () => {
     return await models.roles.findAll();
-})
+}
 
-const get = (async (id) => {
+const get = async (id) => {
     return await models.roles.findByPk(id);
-})
+}
 
-const create = (async (requestBody) => {
+const create = async (requestBody) => {
     return await models.roles.create(requestBody);
-})
+}
 
-const update = (async (id, requestBody) => {
+const update = async (id, requestBody) => {
     return await models.roles.update(requestBody, {
         where: {
             id: id
         }
     })
-})
+}
 
-const remove = (async (id) => {
+const remove = async (id) => {
     return await models.roles.destroy({
         where: {
             id: id
         }
     });
-})
+}
 
-const hasPermission = (async (role, permissionType, permissionFor) => {
+const hasPermission = async (role, permissionType, permissionFor) => {
     rolePermission = await models.roles.findOne({
         where: {
             role_title: role
@@ -42,7 +42,7 @@ const hasPermission = (async (role, permissionType, permissionFor) => {
         }]
     });
     return !(null == rolePermission);
-});
+}
 
 module.exports = {
     getAll,
